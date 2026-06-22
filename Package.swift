3951daa3bd46e5376628380bbcc3999ba38cb830
avgx <agovorovsky@gmail.com>
2026-06-22 12:01:40 +0300

    remove .watchOS

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .tvOS(.v15)
     ],
     products: [
         .library(
